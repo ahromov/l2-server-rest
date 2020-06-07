08308737ee6de07e00a259d40ead19c526fccd87
andri <andri@DESKTOP-EVQGO3R>
2020-06-07 14:20:28 +0300

    Removed

--- db/forum/V1__phpbb_topics_update.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `phpbb_topics`
-	ADD COLUMN `current_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP() AFTER `topic_posts_softdeleted`;
--- sh/freebsd/startWebServer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./WebServer_loop.sh &

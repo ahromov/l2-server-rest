7a10f1f02c167128d857427cc685f7a35fe32657
Andrew <Andrew@DESKTOP-VUUTVQ9>
2020-06-08 12:49:21 +0300

    Moved

--- src/main/java/ua/cc/lajdev/L2ServerWebappApplication.java
+++ src/main/java/ua/cc/lajdev/L2ServerWebappApplication.java
@@ -1,18 +1,17 @@
 package ua.cc.lajdev;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import ua.cc.lajdev.login.service.AccountService;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class L2ServerWebappApplication {
 
-	@Autowired
-	static AccountService accountService;
+	/*
+	 * protected SpringApplicationBuilder configure(SpringApplicationBuilder
+	 * application) { return application.sources(L2ServerWebappApplication.class); }
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(L2ServerWebappApplication.class, args);

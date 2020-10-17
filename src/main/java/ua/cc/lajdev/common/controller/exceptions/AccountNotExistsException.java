44ae7cc50a96aca4e8167b794d2997a5116a78c6
Andrii Hromov <andrii.hromov@gmail.com>
2020-10-17 15:47:40 +0300

    Fixed status code

--- src/main/java/ua/cc/lajdev/common/controller/exceptions/AccountNotExistsException.java
+++ src/main/java/ua/cc/lajdev/common/controller/exceptions/AccountNotExistsException.java
@@ -3,7 +3,7 @@ package ua.cc.lajdev.common.controller.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Account not exists")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Account not exists")
 public class AccountNotExistsException extends RuntimeException {
 
 	private static final long serialVersionUID = -9206412161370700340L;

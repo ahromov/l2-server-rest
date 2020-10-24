8cc30c7ad317c844d8739a8fd36a46068f282796
Andrii Hromov <andrii.hromov@gmail.com>
2020-10-24 12:07:01 +0300

    Added new method for find user by email

--- src/main/java/ua/cc/lajdev/site/repo/UserRepository.java
+++ src/main/java/ua/cc/lajdev/site/repo/UserRepository.java
@@ -1,13 +1,15 @@
 package ua.cc.lajdev.site.repo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ua.cc.lajdev.site.model.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 
-	public User findByUserName(String username);
+	User findByEmail(String email);
+
+	User findByUserName(String username);
 
 }
--- src/main/java/ua/cc/lajdev/site/service/UserService.java
+++ src/main/java/ua/cc/lajdev/site/service/UserService.java
@@ -1,10 +1,13 @@
 package ua.cc.lajdev.site.service;
 
+import ua.cc.lajdev.common.dto.user.UserDto;
 import ua.cc.lajdev.site.model.User;
 
 public interface UserService {
 
-	public User findByUserName(String username);
+	User findByEmail(String email);
+
+	User findByUserName(UserDto user);
 
 	void update(User user);
 

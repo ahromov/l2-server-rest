35f7cf3cbb46d6c159117d31cded677f83b2a571
Andrii Hromov <andrii.hromov@gmail.com>
2020-10-24 12:04:08 +0300

    Reworked mail templates classes

--- src/main/java/ua/cc/lajdev/login/service/impl/mail/MailAccountTemplate.java
+++ src/main/java/ua/cc/lajdev/login/service/impl/mail/MailAccountTemplate.java
@@ -1,10 +1,10 @@
 package ua.cc.lajdev.login.service.impl.mail;
 
-import ua.cc.lajdev.login.dto.user.UserDto;
+import ua.cc.lajdev.common.dto.user.RegistrationDto;
 
 public class MailAccountTemplate extends Template {
 
-	public MailAccountTemplate(UserDto user) {
+	public MailAccountTemplate(RegistrationDto user) {
 		this.subject = "Welcome " + user.login + "!";
 		this.body = "Your account data:\n\tLogin: " + user.login + "\n\tPassword: " + user.password;
 	}
--- src/main/java/ua/cc/lajdev/login/service/impl/mail/MailPasswordTemplate.java
+++ src/main/java/ua/cc/lajdev/login/service/impl/mail/MailPasswordTemplate.java
@@ -1,12 +1,18 @@
 package ua.cc.lajdev.login.service.impl.mail;
 
-import ua.cc.lajdev.login.dto.user.UserDto;
+import ua.cc.lajdev.login.model.Account;
+import ua.cc.lajdev.site.model.User;
 
 public class MailPasswordTemplate extends Template {
 
-	public MailPasswordTemplate(UserDto user) {
-		this.subject = user.login + ", Password succesfuly changed!";
-		this.body = "Your new password: " + user.password;
+	public MailPasswordTemplate(Account account) {
+		this.subject = account.getLogin() + ", Password succesfuly changed!";
+		this.body = "Your new password: " + account.getPassword();
+	}
+
+	public MailPasswordTemplate(User account) {
+		this.subject = account.getUserName() + ", Password succesfuly changed!";
+		this.body = "Your new password: " + account.getPassword();
 	}
 
 }

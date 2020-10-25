96d22d571dcdf68e1b026000a4e28427cc0a8693
Andrii Hromov <andrii.hromov@gmail.com>
2020-10-25 14:00:19 +0200

    Reworked with JQuery

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,43 +1,37 @@
-var newsForm = document.querySelector('#newsForm');
-var newsImage = document.querySelector('#newsImage');
-var newsUploadError = document.querySelector('#newsUploadError');
-var newsUploadSuccess = document.querySelector('#newsUploadSuccess');
+$(document).on('click', "button.green", function() {
+	let formData = new FormData();
 
-function addNews() {
-	let formData = new FormData(newsForm);
+	let title = $("#newsTitle").val();
+	let text = $("#newsText").val();
+	let image = $("#newsImage").prop('files');
 
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", "/news/add");
+	formData.append("image", image[0]);
+	formData.append('newsDto', new Blob([JSON.stringify({
+		"title": title,
+		"text": text
+	})], {
+		type: "application/json"
+	}));
 
-	xhr.send(formData);
+	let prom = fetch('/news/add', {
+		method: 'POST',
+		body: formData
+	});
 
-	xhr.onload = function () {
-		console.log(xhr.responseText);
-
-		var response = JSON.parse(xhr.responseText);
-
-		if (xhr.status == 200) {
-			newsForm.style.display = "none";
-			newsUploadError.style.display = "none";
-			newsUploadSuccess.innerHTML = `<p>${response.id} <br> ${response.title} <br> ${response.text} <br> ${new Date(response.date).toLocaleDateString()} <br> ${new Date(response.date).toLocaleTimeString()} <br> <img style='width: 50px; margin-bottom: 20px' src='data:image/png;base64,${response.image}'> <br> ${response.status}</p> `;
-			newsUploadSuccess.style.display = "block";
-		} else {
-			newsUploadSuccess.style.display = "none";
-			newsUploadError.innerHTML = (response && response.status)
-				|| "Some Error Occurred";
+	prom.then(response => response.json()).then(data => {
+		if (data.status == 400) {
+			$('div.status').html("");
+			$('div.status').append(data.message + '<br>');
+			data.errors.forEach(e => $('div.status').append(e.field + ": " + e.defaultMessage + '<br>').css('color', 'red'));
+			return;
 		}
-	}
-}
 
-newsForm.addEventListener('submit', function (event) {
-	var files = newsImage.files;
-
-	if (files.length === 0) {
-		newsUploadError.innerHTML = "Please select a file";
-		newsUploadError.style.display = "block";
-	}
-
-	addNews();
-
-	event.preventDefault();
-}, true);
+		$('form').trigger('reset');
+		$('div.status').html(`<p>${data.id} <br> 
+								${data.title} <br> 
+								${data.text} <br> 
+								${new Date(data.date).toLocaleDateString()} <br> 
+								${new Date(data.date).toLocaleTimeString()} <br> 
+								<img style='width: 50px; margin-bottom: 20px' src='data:image/png;base64,${data.image}'></p> `).css('color', 'green');
+	});
+})

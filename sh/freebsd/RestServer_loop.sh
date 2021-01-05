5019aeced8cd2217a52c37c07314613575f37dec
andrei.gromov <andrei.gromov@unitbean.com>
2021-01-05 16:34:41 +0200

    Added into prodaction configuration PID file properties Configured maven-assembly. Fixed run scripts Fix jenkins settings

--- sh/freebsd/RestServer_loop.sh
+++ sh/freebsd/RestServer_loop.sh
@@ -5,8 +5,11 @@ pidfile=$2
 
 /usr/bin/gzip log/app.log && mv log/app.log.gz "log/`date +%Y-%m-%d_%H-%M-%S`_app.log.gz"
 #/usr/local/openjdk14/bin/java -Xms128m -Xmx1g -jar l2rest.war --spring.config.location=application.properties &
-/usr/sbin/daemon -f -u ${user} -o /usr/home/andrew/l2-server-rest/log/app.log /usr/local/openjdk14/bin/java -Xms128m -Xmx1g -jar l2rest.war --spring.config.location=application.properties &
-let pid=$!+1
+# /usr/sbin/daemon -f -u ${user} -o /usr/home/andrew/l2-server-rest/log/app.log /usr/local/openjdk14/bin/java -Xms128m -Xmx1g -jar l2rest.war --spring.config.location=application.properties &
+/usr/sbin/daemon -f -u ${user} -o /usr/home/andrew/l2-server-rest/log/app.log /usr/local/openjdk14/bin/java -Xms128m -Xmx1g -jar l2-server-rest-0.0.1-SNAPSHOT.jar
+# let pid=$!+1
 
-echo ${pid} > ${pidfile}
+# echo ${pid} > ${pidfile}
+# cat ${pidfile}
+
 

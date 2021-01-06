3eea6a3e246eba8a9dfa35d1c6183b69f4b0fdd1
Andrii Hromov <andrii.hromov@gmail.com>
2021-01-06 12:15:53 +0200

    Changed hardcoded users home catalog on variable. Fixed right access to

--- sh/freebsd/RestServer_loop.sh
+++ sh/freebsd/RestServer_loop.sh
@@ -7,6 +7,7 @@ pidfile=$2
 #/usr/local/openjdk14/bin/java -Xms128m -Xmx1g -jar l2rest.war --spring.config.location=application.properties &
 # /usr/sbin/daemon -f -u ${user} -o /usr/home/andrew/l2-server-rest/log/app.log /usr/local/openjdk14/bin/java -Xms128m -Xmx1g -jar l2rest.war --spring.config.location=application.properties &
 /usr/sbin/daemon -f -u ${user} -o /usr/home/${user}/l2-server-rest/log/app.log /usr/local/openjdk14/bin/java -Xms128m -Xmx1g -jar l2-server-rest-0.0.1-SNAPSHOT.jar
+chown -R ${user}:${user} /home/${user}/l2-server-rest
 chmod 700 /usr/home/${user}/l2-server-rest/log/app.log
 # let pid=$!+1
 

bab58cfdccf4fc78e8f472d4668b720568759f7c
Andrii Hromov <andrii.hromov@gmail.com>
2021-01-07 11:46:06 +0200

    Fix: package settings * Renamed artifact name * Fixed run-script * Updated .gitignore

--- sh/freebsd/RestServer_loop.sh
+++ sh/freebsd/RestServer_loop.sh
@@ -6,7 +6,7 @@ pidfile=$2
 /usr/bin/gzip log/app.log && mv log/app.log.gz "log/`date +%Y-%m-%d_%H-%M-%S`_app.log.gz"
 #/usr/local/openjdk14/bin/java -Xms128m -Xmx1g -jar l2rest.war --spring.config.location=application.properties &
 # /usr/sbin/daemon -f -u ${user} -o /usr/home/andrew/l2-server-rest/log/app.log /usr/local/openjdk14/bin/java -Xms128m -Xmx1g -jar l2rest.war --spring.config.location=application.properties &
-/usr/sbin/daemon -f -u ${user} -o /usr/home/${user}/l2-server-rest/log/app.log /usr/local/openjdk14/bin/java -Xms128m -Xmx1g -jar l2-server-rest-0.0.1-SNAPSHOT.jar
+/usr/sbin/daemon -f -u ${user} -o /usr/home/${user}/l2-server-rest/log/app.log /usr/local/openjdk14/bin/java -Xms128m -Xmx1g -jar l2-server-rest.jar
 chown -R ${user}:${user} /home/${user}/l2-server-rest
 chmod 700 /usr/home/${user}/l2-server-rest/log/app.log
 # let pid=$!+1
